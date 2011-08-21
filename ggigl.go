65b2671cb43293987e4a6c9b02efcbc82893017a
DeedleFake <yisszev@beckforce.com>
2011-08-21 11:19:48 -0400

    Added both piece colors.

--- ggigl.go
+++ ggigl.go
@@ -1,5 +1,11 @@
 package main
 
+var Pieces map[string]*Piece
+
 func main() {
+	Pieces = make(map[string]*Piece)
+	Pieces["black"] = NewPiece("black")
+	Pieces["white"] = NewPiece("white")
+
 	println("There is no response...")
 }
--- piece.go
+++ piece.go
@@ -1,3 +1,22 @@
 package main
 
-type Piece struct{}
+import(
+	"sdl"
+	"path"
+)
+
+var(
+	PiecePath = path.Join("data", "pieces")
+)
+
+type Piece struct{
+	img *sdl.Surface
+}
+
+func NewPiece(t string) *Piece {
+	p := new(Piece)
+
+	p.img = sdl.Load(path.Join(PiecePath, t))
+
+	return p
+}
