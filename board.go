c5e55f8d6b68c60539f8a85b927e4e5495a17b94
DeedleFake <yisszev@beckforce.com>
2011-08-21 11:02:57 -0400

    gofmt.

--- board.go
+++ board.go
@@ -2,8 +2,8 @@ package main
 
 type BoardSize int
 
-const(
-	Size9x9 BoardSize = 9
+const (
+	Size9x9   BoardSize = 9
 	Size19x19 BoardSize = 19
 )
 
@@ -17,20 +17,20 @@ func NewBoard(size BoardSize) (b *Board) {
 	b = new(Board)
 
 	b.size = int(size)
-	b.pieces = make([]*Piece, b.size * b.size)
+	b.pieces = make([]*Piece, b.size*b.size)
 
 	return
 }
 
-func (b *Board)At(x, y int) *Piece {
+func (b *Board) At(x, y int) *Piece {
 	return b.pieces[(y*b.size)+x]
 }
 
-func (b *Board)place(x, y int, p *Piece) {
+func (b *Board) place(x, y int, p *Piece) {
 	b.pieces[(y*b.size)+x] = p
 }
 
-func (b *Board)Place(x, y int, p *Piece) bool {
+func (b *Board) Place(x, y int, p *Piece) bool {
 	if b.At(x, y) != nil {
 		return false
 	}
--- piece.go
+++ piece.go
@@ -1,4 +1,3 @@
 package main
 
-type Piece struct {
-}
+type Piece struct{}
